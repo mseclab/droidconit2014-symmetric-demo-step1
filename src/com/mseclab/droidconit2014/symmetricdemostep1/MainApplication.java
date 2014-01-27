4e9f17ddea292ce740e67f70186d9b5748a419f5
Roberto Gassira <r.gassira@mseclab.com>
2014-01-27 11:51:29 +0100

    First Import

---
+++ src/com/mseclab/droidconit2014/symmetricdemostep1/MainApplication.java
@@ -0,0 +1,11 @@
+package com.mseclab.droidconit2014.symmetricdemostep1;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+	public MainApplication() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
