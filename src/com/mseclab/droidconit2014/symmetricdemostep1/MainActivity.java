4cf0a8efe1c24b2e8d79057a6c38ff052105a745
Roberto Gassira' <r.gassira@mseclab.com>
2014-01-28 14:27:11 +0100

    Aggiunto librerie.

--- src/com/mseclab/droidconit2014/symmetricdemostep1/MainActivity.java
+++ src/com/mseclab/droidconit2014/symmetricdemostep1/MainActivity.java
@@ -1,15 +1,26 @@
 package com.mseclab.droidconit2014.symmetricdemostep1;
 
+import java.security.Provider;
+import java.security.Security;
+import java.util.Set;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	private TextView outView;
+	private final static String TAG = "DROIDCONIT";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		outView = (TextView) findViewById(R.id.out_view);
+		showProviders();
 	}
 
 	@Override
@@ -17,6 +28,27 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	private void showProviders() {
+		Provider[] providers = Security.getProviders();
+		for (Provider provider : providers) {
+			debug("Provider: " + provider.getName());
+			debug("Version : " + Double.toString(provider.getVersion()));
+			debug("Info    : " + provider.getInfo());
+			Set<Provider.Service> services = provider.getServices();
+			for (Provider.Service service : services) {
+				debug("  algorithm: " + service.getAlgorithm());
+				debug(service.toString());
+				
+			}
+			debug("\n");
+		}
+	}
+
+	private void debug(String message) {
+		Log.v(TAG, message);
+		outView.append(message + "\n");
 	}
 
 }
